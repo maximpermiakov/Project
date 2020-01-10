5ee139242216e08703aeda109c6c825acd446897
OmagaDL <dmitrij.lejnew@yandex.ru>
2020-01-11 02:51:44 +0400

    Механика движения героя

---
+++ Hero_move.py
@@ -0,0 +1,75 @@
+import pygame
+import sys
+import os
+
+pygame.init()
+size = width, heigth = 500, 500
+screen = pygame.display.set_mode(size)
+pygame.display.set_caption("First_Step")
+
+def load_image(name, colorkey=None):
+    fullname = os.path.join('data', name)
+    image = pygame.image.load(fullname)
+    if colorkey is not None:
+        if colorkey == -1:
+            colorkey = image.get_at((0, 0))
+        image.set_colorkey(colorkey)
+    else:
+        image = image.convert_alpha()
+    return image
+
+all_sprites = pygame.sprite.Group()
+x = 50
+y = 450
+
+def print_sprite(x, y, im):
+    sprite_im = pygame.sprite.Sprite()
+    sprite_im.image = load_image(im)
+    sprite_im.rect = sprite_im.image.get_rect()
+    all_sprites.add(sprite_im)
+    sprite_im.rect.x = x
+    sprite_im.rect.y = y
+    return sprite_im
+
+speed = 5
+isJump = False
+jumpCount = 10
+run = True
+while run:
+    pygame.time.delay(50)
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_LEFT] and x > 0:
+        x -= speed
+    if keys[pygame.K_RIGHT] and x < 500 - 45:
+        x += speed
+    if not(isJump):
+        if keys[pygame.K_UP] and y > 0:
+            y -= speed
+        if keys[pygame.K_DOWN] and y < 500 - 45:
+            y += speed
+        if keys[pygame.K_SPACE]:
+            isJump = True
+    else:
+        if jumpCount >= -10:
+            if jumpCount < 0:
+                y += (jumpCount**2) / 2
+            else:
+                y -= (jumpCount**2) / 2
+            jumpCount -= 1
+        else:
+            isJump = False
+            jumpCount = 10
+
+    copio = print_sprite(x, y, "bugs.png")
+    screen.fill((0, 0, 0))
+    all_sprites.draw(screen)
+    all_sprites.remove(copio)
+    pygame.display.update()
+
+
+pygame.quit()
