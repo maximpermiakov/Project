3cc15a11e99a0ef476bb120767f3e68984923671
BahrMikhev <ivan.miheyev@gmail.com>
2020-01-23 03:02:03 +0400

    MASSIVE_UPDATE

--- Hero_move_2.py
+++ Hero_move_2.py
@@ -3,6 +3,7 @@ import sys
 import os
 import random
 
+
 def load_image(name, colorkey=None):
     fullname = os.path.join('data', name)
     image = pygame.image.load(fullname)
@@ -14,12 +15,13 @@ def load_image(name, colorkey=None):
         image = image.convert_alpha()
     return image
 
+
 pygame.init()
 size = width, height = 900, 700
-screen = pygame.display.set_mode(size, pygame.FULLSCREEN)
+screen = pygame.display.set_mode(size)
 pygame.display.set_caption("First_Step")
-BACK = ["game_fon.jpg", "game_fon_1.jpg"] #фоны для разных уровней
-heroes_stats = [(1000, 120, 4, 300), (700, 70, 6, 500)] #здоровье, урон, скорость, стамина
+BACK = ["game_fon.jpg", "game_fon_1.jpg"]  # фоны для разных уровней
+heroes_stats = [(1000, 120, 4, 300), (700, 70, 6, 500)]  # здоровье, урон, скорость, стамина
 corvo_bull = load_image("corvo_bul.png")
 corvo_bull = pygame.transform.scale(corvo_bull, (20, 40))
 copio_bull = load_image("copio_bull.png")
@@ -27,6 +29,7 @@ copio_bull = pygame.transform.scale(copio_bull, (20, 10))
 bullet_img = [corvo_bull, copio_bull]
 bul_v = [10, 15]
 k = 0
+
 
 def terminate():
     pygame.quit()
@@ -47,6 +50,7 @@ class Bullet:
         else:
             return False
 
+
 def print_text(mess, x, y, font_color, font_size=40):
     font_type = pygame.font.Font(None, font_size)
     text = font_type.render(mess, True, font_color)
@@ -54,18 +58,21 @@ def print_text(mess, x, y, font_color, font_size=40):
     t_h = text.get_height()
     screen.blit(text, (x, y))
 
+
 def bul_move(x, y, bul_speed, mass):
     mass.append(bullet_img[k])
     if 0 < x < width:
         for i in mass:
             if left or left_s:
-                screen.blit(i, (x, y+20))
+                screen.blit(i, (x, y + 20))
                 x -= bul_speed
             elif right or right_s:
-                screen.blit(i, (x+50, y+20))
+                screen.blit(i, (x + 50, y + 20))
                 x += bul_speed
             if x <= 0 or x >= width:
                 mass.remove(i)
+
+
 n = 0
 game_fon = pygame.transform.scale(load_image(BACK[n]), (width, height))
 screen.blit(game_fon, (0, 0))
@@ -90,6 +97,8 @@ animCount = 0
 weapon = False
 clock = pygame.time.Clock()
 tp_rad = 50
+
+
 # функция для анимации
 def drawWindow():
     global animCount, left, right, right_s, left_s, k
@@ -121,11 +130,11 @@ def drawWindow():
         bullet_img = [corvo_bull, copio_bull]
         all_btn_bullets.append(bullet_img[k])
         for bullet in all_btn_bullets:
-            screen.blit(bullet, (bul_x + 15, bul_y+30))
-
+            screen.blit(bullet, (bul_x + 15, bul_y + 30))
 
     print_text(str(heroes_stats[k][0]), 10, 5, (255, 0, 0))
     pygame.display.update()
+
 
 def print_sprite(x, y, im):
     sprite_im = pygame.sprite.Sprite()
@@ -136,17 +145,20 @@ def print_sprite(x, y, im):
     sprite_im.rect.y = y
     return sprite_im
 
+
 corvo_r = [load_image("corvo_1.png"), load_image("corvo_2.png"),
-          load_image("corvo_3.png"), load_image("corvo_4.png"),
-          load_image("corvo_5.png")]
+           load_image("corvo_3.png"), load_image("corvo_4.png"),
+           load_image("corvo_5.png")]
 corvo_l = [load_image("corvo_1_left.png"), load_image("corvo_2_left.png"),
-          load_image("corvo_3_left.png"), load_image("corvo_4_left.png"), load_image("corvo_5_left.png")]
+           load_image("corvo_3_left.png"), load_image("corvo_4_left.png"),
+           load_image("corvo_5_left.png")]
 corvo_s_r = load_image("corvo_4.png")
 corvo_s_l = load_image("corvo_4_left.png")
 copio_r = [load_image("copio_1.png"), load_image("copio_2.png"),
            load_image("copio_3.png"), load_image("copio_4.png"), load_image("copio_1.png")]
 copio_l = [load_image("copio_1_left.png"), load_image("copio_2_left.png"),
-           load_image("copio_3_left.png"), load_image("copio_4_left.png"), load_image("copio_1_left.png")]
+           load_image("copio_3_left.png"), load_image("copio_4_left.png"),
+           load_image("copio_1_left.png")]
 copio_s_r = load_image("copio_1.png")
 copio_s_l = load_image("copio_1_left.png")
 hero_move = [(corvo_r, corvo_l), (copio_r, copio_l)]
@@ -158,7 +170,6 @@ speed = heroes_stats[k][2]
 all_btn_bullets = []
 down = True
 while run:
-
     clock.tick(35)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -187,7 +198,7 @@ while run:
                     all_btn_bullets.remove(bullet)
 
     if right or right_s:
-            bul_x += bul_v[k]
+        bul_x += bul_v[k]
     elif left or left_s:
         bul_x -= bul_v[k]
 
@@ -229,7 +240,7 @@ while run:
     if jp >= 200:
         lost = True
 
-    if not(isJump):
+    if not (isJump):
         if jp <= heroes_stats[k][3]:
             jp += 5
         if (keys[pygame.K_DOWN] or keys[pygame.K_s]):
@@ -241,9 +252,9 @@ while run:
     elif isJump and jp > 0:
         if jumpCount >= -8:
             if jumpCount < 0:
-                y += (jumpCount**2) / 3
+                y += (jumpCount ** 2) / 3
             else:
-                y -= (jumpCount**2) / 3
+                y -= (jumpCount ** 2) / 3
             jumpCount -= 1
         else:
             isJump = False
